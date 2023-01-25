b28df84e23a9fb27a5c29466d42d4f5f06aca1a3
Plasmat1x <damaghack@gmail.com>
2023-01-25 12:10:49 +0700

    update app structure

--- App/Forms/MainForm.Designer.cs
+++ App/Forms/MainForm.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace WFATestZone
+﻿namespace CourseProject
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
--- App/Forms/MainForm.cs
+++ App/Forms/MainForm.cs
@@ -7,12 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.Sql;
 
-namespace WFATestZone
+namespace CourseProject
 { 
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
         }

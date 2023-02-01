5555b2e085a3f876904d10d2ace0ffee9334e2db
Mikhail Mryasov <damaghack@gmail.com>
2023-02-02 01:05:03 +0700

    update uml

--- App/Model/CF/ItemCountry.cs
+++ App/Model/CF/ItemCountry.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CourseProject.Model.DBF;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,14 @@ namespace CourseProject.Model
 {
     public class ItemCountry
     {
-        public int ItemCountryId { get; set; }
-        public Item Item { get; set; }
-        public Country Country { get; set; }
+        public int ItemId { get; set; }
+        public int CountryId { get; set; }
+        public int QuantityId { get; set; }
+        public int FailsId { get; set; }
+
+        public virtual Countries Countries { get; set; }
+        public virtual Fails Fails { get; set; }
+        public virtual Items Items { get; set; }
+        public virtual Quantities Quantities { get; set; }
     }
 }

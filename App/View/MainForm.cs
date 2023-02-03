41747d5db0134afcd988304d3c5460c08459c279
Mikhail Mryasov <damaghack@gmail.com>
2023-02-04 02:11:38 +0700

    Complete item form

--- App/View/MainForm.cs
+++ App/View/MainForm.cs
@@ -12,41 +12,53 @@ using System.Data.SqlClient;
 using CourseProject.Model;
 using CourseProject.View;
 using System.Data.Entity;
+using System.Xml.Linq;
 
 namespace CourseProject
 { 
     public partial class MainForm : Form
     {
         AppDbContext context;
-
+        Model.Item current;
         public MainForm()
         {
             InitializeComponent();
-            initElements();  
+            initElements();
+
+            UpdateForm();
         }
 
         private void initElements()
         {
-            
-            context = new AppDbContext();
-
             btAddItem.Click += addItem;
-
-            var items = context.Items.ToList();
-
-            ItemsGrid.DataSource = items;
+            ItemsGrid.SelectionChanged += ItemsGrid_SelectionChanged;
         }
 
         private void addItem(object sender, EventArgs e)
         {
-            ItemForm itemform = new ItemForm(formMode.ADD);
-            itemform.ShowDialog();
+            ItemForm itemform = new ItemForm(null);
+            if(itemform.ShowDialog() == DialogResult.OK)
+                UpdateForm();
         }
 
         private void btInspectItem_Click(object sender, EventArgs e)
         {
-            ItemForm itemform = new ItemForm(formMode.INSPECT);
-            itemform.ShowDialog();
+            ItemForm itemform = new ItemForm(current);
+            if (itemform.ShowDialog() == DialogResult.OK)
+                UpdateForm();
+
+        }
+
+        private void ItemsGrid_SelectionChanged(object sender, EventArgs e)
+        {
+            current = ((Model.Item)ItemsGrid.CurrentRow.DataBoundItem);
+        }
+
+        private void UpdateForm()
+        {
+            context = new AppDbContext();
+            var items = context.Items.ToList();
+            ItemsGrid.DataSource = items;
         }
     }
 }
